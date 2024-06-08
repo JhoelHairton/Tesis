f4f08109078a5aaffefa074add73b82df8e1a225
JhoelHairton <jhoelhairton770@gmail.com>
2024-06-08 12:44:51 -0500

    first commit

---
+++ Servicio-evaluaciones/src/main/java/com/example/servicioevaluaciones/ServicioEvaluacionesApplication.java
@@ -0,0 +1,13 @@
+package com.example.servicioevaluaciones;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioEvaluacionesApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioEvaluacionesApplication.class, args);
+	}
+
+}
---
+++ servicio-jurados/src/main/java/com/example/serviciojurados/ServicioJuradosApplication.java
@@ -0,0 +1,13 @@
+package com.example.serviciojurados;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class ServicioJuradosApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(ServicioJuradosApplication.class, args);
+	}
+
+}
